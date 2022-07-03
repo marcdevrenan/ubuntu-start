9ef7008e6ce6949150582957bd395b36788b1f01
Renan Marcilio <68852560+marcdevrenan@users.noreply.github.com>
2022-07-03 16:57:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # Poetry
 sudo apt update
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 
 # Docker
 sudo apt update
